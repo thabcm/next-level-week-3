cb851dc0c954657cdaf8a60aca115e1f84fbeabf
thabcm <thalitahelp@gmail.com>
2020-10-15 14:45:24 -0300

    Abstrai componentes e finaliza o front-end

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+import MapIcon from '../utils/mapIcon';
 
 function OrphanagesMap() {
     return (
@@ -32,9 +33,22 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            
+                <Marker 
+                    icon={MapIcon}
+                    position={[-8.0405575, -34.8990065]}>
+
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF"></FiArrowRight>
+                        </Link>
+                    </Popup>
+                </Marker>
+
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
---
+++ web/src/utils/mapIcon.ts
@@ -0,0 +1,14 @@
+import Leaftlet from 'leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+const MapIcon = Leaftlet.icon({
+    iconUrl: mapMarkerImg,
+  
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [0, -60]
+})
+
+export default MapIcon;
+
